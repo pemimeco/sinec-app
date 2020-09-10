781dd47c33606c330b5293b447534b4d236f4b7d
pedro <pemimeco@gmail.com>
2020-09-10 09:41:44 -0400

    obtener datos del asegurado

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,14 +1,46 @@
 const express = require('express')
 const router = express.Router()
-const users = require('../queries/users')
+const sql = require('mssql')
+const sqlConfig = require('../database')
+const users = require('../queries/users');
 
 
 //-------------ROUTES-------------------
-router.get('/', function (req, res, next) {
-    res.render('index', {
-        title: "Home",
-        userData: req.user
-    });
+router.get('/buscarAsegurado', (req, res, next) => {
+    res.render('index');
+});
+router.post('/buscarAsegurado', async (req, res, next) => {
+    // console.log(req.body.edtBuscar)
+    if (req.body.edtBuscar == '') {
+        res.render('index', {
+            message: 'Ingrese un id valido'
+        });
+    }
+    await sql.connect(sqlConfig, (err) => {
+        if (err) {
+            console.log(err)
+        }
+        var request = new sql.Request();
+        request.query(`select * from asegurados2 where cod = '${req.body.edtBuscar}'`, (err, result) => {
+            if (err) {
+                console.log(err)
+            }
+            const response = result.recordset[0]
+            res.render('index', {
+                message: 'Usuario Obtenido',
+                id: req.body.edtBuscar,
+                nombre: response.name,
+                apellido: response.name,
+                empresa: 'test',
+                fec_ing: 'test',
+                matricula: 'test',
+                fec_nac: 'test'
+
+            })
+            console.log(result.recordset)
+        });
+    })
+
 });
 //----------------------------------------------
 //----------------API---------------------------
